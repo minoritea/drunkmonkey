471972efff75ed1bc40c0590660beb770f811cb2
minoritea <m.tokuda@aol.jp>
2013-11-07 19:51:10 +0900

    add some comments

--- sample/app.rb
+++ sample/app.rb
@@ -2,7 +2,9 @@ require "puma"
 require "rack/handler/puma"
 require "drunkmonkey"
 
+#stock sessions on the global space
 sockets = {}
+
 Rack::Handler::Puma.run(Rack::Builder.new{
   root = File.dirname(__FILE__)
   use DrunkMonkey::Middleware do
